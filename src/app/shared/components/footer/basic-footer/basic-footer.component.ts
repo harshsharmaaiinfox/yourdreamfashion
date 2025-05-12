86944811126903fc1f5a7816f2cf379b7696e52f
harshsharmaaiinfox <harsh.sharma@aiinfox.com>
2025-05-12 20:21:49 +0530

    dashboard and form design

--- src/app/shared/components/footer/basic-footer/basic-footer.component.ts
+++ src/app/shared/components/footer/basic-footer/basic-footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Option } from '../../../../shared/interface/theme-option.interface';
 import { Footer } from '../../../../shared/interface/theme.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-footer',
@@ -14,10 +15,44 @@ export class BasicFooterComponent {
 
   public active: { [key: string]: boolean } = {
     categories: false,
-    useful_link: false
+    useful_link: false,
+    help_center: false
   };
+
+  subscribeForm: FormGroup;
+  submitted = false;
+  subscribeSuccess = false;
+  subscribeError = '';
+
+  constructor(private fb: FormBuilder) {
+    this.subscribeForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
 
   toggle(value: string){
     this.active[value] = !this.active[value];
+  }
+
+  onSubscribe() {
+    this.submitted = true;
+    this.subscribeSuccess = false;
+    this.subscribeError = '';
+    
+    if (this.subscribeForm.valid) {
+      // Here you would typically call a service to handle the subscription
+      // For demo purposes, we'll simulate a successful subscription
+      setTimeout(() => {
+        this.subscribeSuccess = true;
+        this.subscribeForm.reset();
+        this.submitted = false;
+      }, 1000);
+    } else {
+      this.subscribeError = 'Please enter a valid email address.';
+    }
+  }
+
+  get emailControl() {
+    return this.subscribeForm.get('email');
   }
 }
